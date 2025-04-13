898da027d5fcb3ee913b441b80243be2d922174d
Li <li@silica.codes>
2025-04-13 12:05:32 +1200

    Add files via upload

---
+++ LibMonoBridge.hpp
@@ -0,0 +1,9 @@
+#ifndef LIB_MONO_BRIDGE_H
+#define LIB_MONO_BRIDGE_H 1
+
+#include <PssCrypto.h>
+#include <PssCryptoContext.h>
+#include <PssCryptoCallbacks.h>
+#include <PssCryptoSeek.h>
+
+#endif
